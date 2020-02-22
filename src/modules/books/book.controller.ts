4b359f0bd3f23d458f8004eb97435d2a4dc23f58
Semicolon10 <ekanayakeindrajith@gmail.com>
2020-02-22 19:12:14 +0530

    search one book by id and get all books

--- src/modules/books/book.controller.ts
+++ src/modules/books/book.controller.ts
@@ -3,20 +3,16 @@ import Book from './book.schema';
 
 export const getBooks = async (req: Request, res: Response): Promise<void> => {
   const books = await Book.find({});
-  // TODO: your implementation here.
+  res.json(books);
 };
 
-export const getBookById = async (
-  req: Request,
-  res: Response,
-): Promise<void> => {
+export const getBookById = async (req: Request,res: Response,): Promise<void> => {
   const books = await Book.findById(req.params.bookId);
-  // TODO: your implementation here.
+  res.json(books);
 };
 
 export const addBook = async (req: Request, res: Response): Promise<void> => {
   let book = new Book(req.body);
   book = await book.save();
   res.status(201).json(book);
-  // TODO: your implementation here.
 };
